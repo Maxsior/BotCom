1c2c5d656cb055c2568f0743bf78a9f65efc98d2
Maxsior <maxsior@mail.ru>
2019-01-28 10:50:39 +0500

    Добавлен вызов обработчика запросов от ВКонтакте

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import json
 from config import keys
-
-# TODO import module for vk
-
+from social import vk
 # TODO import module for telegram
+
 
 # WSGI main
 def application(environ, start_response):
@@ -10,11 +10,13 @@ def application(environ, start_response):
     path = environ.get('PATH_INFO')
     if method == 'POST':
         start_response('200 OK', [('Content-Type', 'application/json')])
+        size = int(environ.get("CONTENT_LENGTH"))
+        body = environ.get("wsgi.input").read(size)
+        data = json.loads(body.decode('utf-8'))
         if path == '/vk':
-            # vk.send_message(msg = text, to = username, auth = keys['vk'])
-            pass
+            vk.parse(data)
         elif path == '/telegram':
-            # telegram.send_message(msg = text, to = username, auth = keys['tg'])
+            # telegram.parse(data)
             pass
         yield b''
     else:
