a3bf92a25038a94fe2e7661cfb8133560b8aa062
Kirill Heroes <kirill_o@list.ru>
2019-02-06 14:51:22 +0500

    Добавлены клавиатуры для бота ВКонтакте

--- social/vk.py
+++ social/vk.py
@@ -2,11 +2,12 @@ from config import keys
 import storage
 from urllib.parse import urlencode
 from urllib.request import urlopen
+import json
 
-# TODO отлов ошибок в функции parse
-# TODO переделать на Long pull
 
 def send_message(uid, msg):
+    with open("vk_keyboards.json") as f:
+        keyboards = json.load(f)
     api_url = 'https://api.vk.com/method/messages.send?'
     query = urlencode({
         "user_id": storage.get_real_id(uid, 'vk'),
