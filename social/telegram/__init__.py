71546297fffa3462fe83de220a525ec5b4e3e625
RobinZone3 <ananjev.alexandr2012@yandex.ru>
2019-02-11 11:09:14 +0500

    Доделали парсес запросов и отправку сообщений

--- social/telegram/__init__.py
+++ social/telegram/__init__.py
@@ -1,9 +1,38 @@
 from config import keys
+import storage
+from urllib.parse import urlencode
+from urllib.request import urlopen
 
 
-def send_message():
-    pass
+# TODO Написать методы для отправки и парса сообщение как в vk.py
+
+
+def send_message(real_id, msg):
+    api_url = 'https://api.telegram.org/bot tMe'
+
+    api_url = f"https://api.telegram.org/bot{keys['telegram']}/sendMessage?"
+    query = urlencode({
+        "chat_id": real_id,
+        "text": msg
+    })
+    api_url += query
+
+    # TODO обработать ответ
+
+    with urlopen(api_url) as res:
+        print(res.read().decode('utf-8'))
+
 
 
 def parse(data):
-    pass
+    if 'message' in data:
+        msg = data['message']
+
+        return {
+            'real_id': msg['from']['id'],
+            'msg': msg['text'],
+            'social': 'telegram'
+        }
+    else:
+        return ''
+
